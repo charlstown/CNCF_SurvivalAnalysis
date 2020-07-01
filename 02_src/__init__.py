f32f0253598c059f1a0b78559b389342e78437da
charlstown <carlosgranden@gmail.com>
2020-07-01 22:35:29 +0200

    New changes

---
+++ 02_src/__init__.py
@@ -0,0 +1,3 @@
+# This folder holds the libraries to load and find files in elasticsearch.
+# 1. elastic_loader: load all the data, import and export mappings.
+# 2. elastic_finder: make queries easily.
---
+++ 02_src/elastic_loader.py
@@ -0,0 +1,42 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+import json
+import time
+import math
+
+
+def load_json(path):
+    print('Loading json file...', '\n')
+    start = time.time()
+    path_open = open(path, 'r').readlines()
+    count = 0
+    file = []
+    try:
+        for i, line in enumerate(path_open):
+            file.append(json.loads(line))
+            percent = math.trunc(((i+1)/len(path_open))*100)
+            if (percent >= 25 and count == 0) or (percent > 50 and count == 1) or (percent > 75 and count == 2) or percent == 100:
+                count += 1
+                end = round(time.time() - start, 2)
+                print("{} percent completed: {} documents in {}s".format(percent, i+1, end))
+        print('\n')
+        print('json file was loaded succesfully!')
+    except Exception as e:
+        print(e)
+    return file
+
+
+def upload_to_index(client, index_name, data):
+    start = time.time()
+    print('Starting upload...', '\n')
+    count = 0
+    for i, doc in enumerate(data):
+        client.index(index=index_name, id=doc['_id'], body=doc['_source'])
+        percent = math.trunc(((i+1)/len(data))*100)
+        if (percent >= 25 and count == 0) or (percent > 50 and count == 1) or (
+                percent > 75 and count == 2) or percent == 100:
+            count += 1
+            end = round(time.time() - start, 2)
+            print("{} percent completed: {} documents in {}s".format(percent, i + 1, end))
+    print('\n')
+    print('Data was uploaded succesfully!')
